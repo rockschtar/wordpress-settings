009eb70743821d7aeb6b3b6064274c0049687506
Stefan Helmer <rockschtar@gmail.com>
2018-12-08 23:30:30 +0100

    feature test

--- wordpress-settings-mu-loader.php
+++ wordpress-settings-mu-loader.php
@@ -10,3 +10,8 @@ License:      MIT License
 */
 
 require_once 'vendor/autoload.php';
+
+
+function rwps_create_settings(\Rockschtar\WordPress\Settings\Models\Page $page): void {
+    \Rockschtar\WordPress\Settings\Controller\WordPressSettings::init($page);
+}
