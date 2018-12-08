3caccef6c0c04f7af0e1c17983a311fbfe6cdd22
Stefan Helmer <rockschtar@gmail.com>
2018-12-09 00:10:02 +0100

    better plugin init

--- wordpress-settings.php
+++ wordpress-settings.php
@@ -8,9 +8,12 @@ Author:       Stefan Helmer
 Author URI:   https://validio.io/
 License:      MIT License
 */
+define('RWPS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
+if (file_exists(RWPS_PLUGIN_DIR . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php')) {
+    require_once 'vendor/autoload.php';
+}
 
-require_once 'vendor/autoload.php';
 require_once 'bootstrap.php';
 
 add_action('init', function () {
