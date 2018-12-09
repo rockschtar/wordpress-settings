afa1c869901e024b9c2b1efd31b7cb32fb559f07
Stefan Helmer <87191+rockschtar@users.noreply.github.com>
2018-12-09 14:54:01 +0100

    modified select box

---
+++ src/Models/FieldListItems.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stefan Helmer
+ * Date: 09.12.2018
+ * Time: 14:50
+ */
+
+namespace Rockschtar\WordPress\Settings\Models;
+
+
+use Rockschtar\TypedArrays\TypedArray;
+
+class FieldListItems extends TypedArray {
+    public function current(): FieldListItem {
+        return parent::current();
+    }
+
+
+    public function getType(): string {
+        return FieldListItem::class;
+    }
+
+    protected function isDuplicate($value): bool {
+        return false;
+    }
+}
