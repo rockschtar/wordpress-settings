c4367d81dd6e11fb6aaa9e6bbc241c20f039189b
Stefan Helmer <sh@validio.de>
2018-12-10 09:43:13 +0100

    fixed multi use

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
+if (function_exists('add_action')) {
+    add_action('init', function () {
+        do_action('rwps_create_settings');
+    }, 1);
+}
+
 function rwps_create_settings(\Rockschtar\WordPress\Settings\Models\SettingsPage $page): void {
     \Rockschtar\WordPress\Settings\Controller\WordPressSettings::init($page);
 }
