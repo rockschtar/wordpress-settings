9a4aaf8fef29c1251fd8fbdac100b6e7c6b06505
Stefan Helmer <87191+rockschtar@users.noreply.github.com>
2019-05-27 15:01:41 +0200

    added select2 box

--- src/Fields/Select2Box.php
+++ src/Fields/Select2Box.php
@@ -2,16 +2,27 @@
 
 namespace Rockschtar\WordPress\Settings\Fields;
 
-
 use Rockschtar\WordPress\Settings\Models\AssetScript;
+use Rockschtar\WordPress\Settings\Models\AssetStyle;
 
 class Select2Box extends SelectBox {
 
-
     /**
      * Select2Box constructor.
+     * @param string $id
      */
-    public function __construct() {
-        $d = new AssetScript('rswps-select2-script', RWPS_PLUGIN_URL)
+    public function __construct(string $id) {
+        parent::__construct($id);
+
+        $this->addCssClass('rswp-select2');
+
+        $this->addAsset(new AssetStyle('rswps-select2-style', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css'));
+
+        $select2_script = new AssetScript('rswps-select2-script', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js', 'false', ['jquery']);
+        $select2_script->addInlineScript("jQuery(function() {
+            jQuery('.rswp-select2').select2();
+        });");
+
+        $this->addAsset($select2_script);
     }
 }
