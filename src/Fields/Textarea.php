c3e2874a1a1ee1b1fad056363b4f028bac78d13c
Stefan Helmer <87191+rockschtar@users.noreply.github.com>
2019-07-01 13:52:02 +0200

    modified textarea

--- src/Fields/Textarea.php
+++ src/Fields/Textarea.php
@@ -1,15 +1,136 @@
 <?php
-/**
- * @author: StefanHelmer
- */
-
 namespace Rockschtar\WordPress\Settings\Fields;
 
-
 use Rockschtar\WordPress\Settings\Models\Field;
-use Rockschtar\WordPress\Settings\Models\HTMLTag;
 
 class Textarea extends Field {
+
+    /**
+     * @var bool
+     */
+    private $autofocus = false;
+
+    /**
+     * @var bool
+     */
+    private $dirname = false;
+
+    /**
+     * @var int
+     */
+    private $cols = 80;
+
+    /**
+     * @var int
+     */
+    private $rows = 10;
+
+    /**
+     * @var int|null
+     */
+    private $maxlength;
+
+    /**
+     * @var string
+     */
+    private $wrap;
+
+    /**
+     * @return bool
+     */
+    public function isAutofocus(): bool {
+        return $this->autofocus;
+    }
+
+    /**
+     * @param bool $autofocus
+     * @return Textarea
+     */
+    public function setAutofocus(bool $autofocus): Textarea {
+        $this->autofocus = $autofocus;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCols(): int {
+        return $this->cols;
+    }
+
+    /**
+     * @param int $cols
+     * @return Textarea
+     */
+    public function setCols(int $cols): Textarea {
+        $this->cols = $cols;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRows(): int {
+        return $this->rows;
+    }
+
+    /**
+     * @param int $rows
+     * @return Textarea
+     */
+    public function setRows(int $rows): Textarea {
+        $this->rows = $rows;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getMaxlength(): ?int {
+        return $this->maxlength;
+    }
+
+    /**
+     * @param int|null $maxlength
+     * @return Textarea
+     */
+    public function setMaxlength(?int $maxlength): Textarea {
+        $this->maxlength = $maxlength;
+        return $this;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getWrap(): string {
+        return $this->wrap;
+    }
+
+    /**
+     * @param string $wrap
+     * @return Textarea
+     */
+    public function setWrap(string $wrap): Textarea {
+        $this->wrap = $wrap;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDirname(): bool {
+        return $this->dirname;
+    }
+
+    /**
+     * @param bool $dirname
+     * @return Textarea
+     */
+    public function setDirname(bool $dirname): Textarea {
+        $this->dirname = $dirname;
+        return $this;
+    }
 
     /**
      * @param $current_value
@@ -18,19 +139,29 @@ class Textarea extends Field {
      */
     public function inputHTML($current_value, array $args = []): string {
 
-        return $this->getHTMLTag($current_value)->buildTag();
-    }
+        $textareatTemplate = '<textarea id="{id}" name="{name}" rows="{rows}" cols="{cols}" {attributes}>{value}</textarea>';
 
-    /**
-     * @param $current_value
-     * @return HTMLTag
-     */
-    public function getHTMLTag($current_value): HTMLTag {
+        $textareatTemplate = str_replace(array('{id}', '{name}', '{rows}', '{cols}', '{value}'), array($this->getId(),
+                                                                                                       $this->getId(),
+                                                                                                       $this->getRows(),
+                                                                                                       $this->getCols(),
+                                                                                                       $current_value), $textareatTemplate);
+        $attributes = [];
 
-        $html_tag = parent::getHTMLTag($current_value);
-        $html_tag->setTag('textarea');
-        $html_tag->setAttribute('type', 'null');
+        if ($this->isAutofocus()) {
+            $attributes[] = 'autofocus';
+        }
 
-        return apply_filters('rwps_html_tag', $html_tag, $this->getId());
+        if ($this->isDirname()) {
+            $attributes[] = 'dirname="' . $this->getId() . '.dir"';
+        }
+
+        if ($this->getMaxlength()) {
+            $attributes[] = 'maxlength="' . $this->getMaxlength() . '"';
+        }
+
+        $textarea = str_replace('{attributes}', implode(' ', $attributes), $textareatTemplate);
+
+        return $textarea;
     }
 }
