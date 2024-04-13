45fe2aadebc5fbc791ec80fa62348fecf8e24a92
Stefan Helmer <87191+rockschtar@users.noreply.github.com>
2024-04-13 12:23:45 +0200

    Refactor Wordpress AjaxButton.js and add package-lock.json
    Converted code in AjaxButton.js to a more modern JavaScript style. Improved code readability, corrected naming convention inconsistencies and removed unnecessary dependencies. Also added package-lock.json, which provides version information about the current state of the node modules.

--- js/UploadFile.js
+++ js/UploadFile.js
@@ -1,26 +1,22 @@
-require('jquery');
+require('jquery')
 jQuery(document).ready(function ($) {
+  const RWPSUploadFile = (function () {
+    function init() {
+      jQuery('.rwps-file-upload-delete').bind('click', function () {
+        const field = $(this)
+        const fieldId = field.data('field-id')
+        jQuery('#' + fieldId).val('')
+        jQuery('#' + fieldId + '-file-link').remove()
+        field.remove()
 
-    const RWPSUploadFile = (function () {
+        return false
+      })
+    }
 
-        function init () {
+    return {
+      init
+    }
+  })()
 
-            jQuery('.rwps-file-upload-delete').bind('click', function () {
-                const field = $(this);
-                const fieldId = field.data('field-id');
-                jQuery('#' + fieldId).val('');
-                jQuery('#' + fieldId + '-file-link').remove();
-                field.remove();
-
-                return false;
-            });
-        }
-
-        return {
-            init: init,
-        };
-
-    })();
-
-    RWPSUploadFile.init();
-});
+  RWPSUploadFile.init()
+})
