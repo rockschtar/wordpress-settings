4eb21f2f0d79f8bf929039c3dc8be20c31901ec1
Stefan Helmer <rockschtar@proton.me>
2024-04-13 12:23:45 +0200

    Refactor Wordpress AjaxButton.js and add package-lock.json
    Converted code in AjaxButton.js to a more modern JavaScript style. Improved code readability, corrected naming convention inconsistencies and removed unnecessary dependencies. Also added package-lock.json, which provides version information about the current state of the node modules.

--- js/AjaxButton.js
+++ js/AjaxButton.js
@@ -1,134 +1,138 @@
-require('jquery');
+require('jquery')
 jQuery(document).ready(function ($) {
+  const RWPSAjaxButtons = (function () {
+    const ajaxNonce = rwps_ajax_button.nonce
 
-    const RWPSAjaxButtons = (function () {
+    function init() {
+      jQuery('.rwps-ajax-button').bind('click', function () {
+        const button = $(this)
 
-        const ajax_nonce = rwps_ajax_button.nonce;
+        const buttonText = button.html()
+        const labelWait = button.data('wait-text')
+        const labelSuccess = button.data('label-success')
+        const labelError = button.data('label-success')
+        const callbackSuccess = button.data('callback-success')
+        const callbackError = button.data('callback-error')
+        const callbackDone = button.data('callback-done')
 
-        function init () {
+        const fieldData = {}
+        const form = button.closest('form')
+        const fields = form.find('input, select')
+        const fieldsExcluded = [
+          'action',
+          '_wpnonce',
+          '_wp_http_referer',
+          'submit'
+        ]
 
-            jQuery('.rwps-ajax-button').bind('click', function () {
-
-                const button = $(this);
-
-                const button_text = button.html();
-                const label_wait = button.data('wait-text');
-                const label_success = button.data('label-success');
-                const label_error = button.data('label-success');
-                const callback_success = button.data('callback-success');
-                const callback_error = button.data('callback-error');
-                const callback_done = button.data('callback-done');
-
-                const field_data = {};
-                const form = button.closest('form');
-                const fields = form.find('input, select');
-                const excluded_fields = [
-                    'action',
-                    '_wpnonce',
-                    '_wp_http_referer',
-                    'submit'];
-
-                fields.each(function () {
-                    const name = $(this).attr('name');
-                    let id = $(this).attr('id');
-                    const value = $(this).val();
-                    if (jQuery.inArray(name, excluded_fields) === -1) {
-
-                        if (id === undefined) {
-                            id = name;
-                        }
-
-                        field_data[id] = value;
-                    }
-                });
-
-                button.html(label_wait);
-                button.attr('disabled', 'disabled');
-
-                jQuery.ajax({
-                    type: 'POST',
-                    url: ajaxurl,
-                    dataType: 'json',
-                    data: {
-                        nonce: ajax_nonce,
-                        action: 'rwps_ajax_button_' + button.attr('id'),
-                        fields: field_data,
-                    },
-
-                }).fail(function (jqXHR, textStatus, errorThrown) {
-                    if (label_error === '') {
-                        if (typeof jqXHR.responseJSON === 'object' &&
-                          typeof jqXHR.responseJSON.data === 'string') {
-                            button.html(jqXHR.responseJSON.data);
-                        }
-                        else {
-                            button.html(button_text);
-                        }
-                    }
-                    else {
-                        button.html(label_error);
-                    }
-
-                    if (callback_error !== '' && callback_error !== undefined) {
-                        executeCallback(callback_error, window, jqXHR,
-                          textStatus, errorThrown);
-                    }
-                }).done(function (response, textStatus, jqXHR) {
-                    if (label_success === '') {
-
-                        if (typeof response.data === 'string') {
-                            button.html(response.data);
-                        }
-                        else {
-                            button.html(button_text);
-                        }
-
-                    }
-                    else {
-                        button.html(label_success);
-                    }
-
-                    if (callback_success !== '' && callback_success !==
-                      undefined) {
-                        console.log('cb1', callback_success);
-                        executeCallback(callback_success, window, response,
-                          textStatus, jqXHR);
-                    }
-
-                }).always(function (data_jqXHR, textStatus, jqXHR_errorThrown) {
-                    setTimeout(function () {
-                        button.removeAttr('disabled');
-                        button.html(button_text);
-                    }, 3000);
-
-                    if (callback_done !== '' && callback_done !== undefined) {
-                        executeCallback(callback_done, window, data_jqXHR,
-                          textStatus, jqXHR_errorThrown);
-                    }
-                });
-            });
-
-        }
-
-        function executeCallback (callback, context) {
-
-            const args = Array.prototype.slice.call(arguments, 2);
-            const namespaces = callback.split('.');
-            const func = namespaces.pop();
-
-
-            for (let i = 0; i < namespaces.length; i++) {
-                context = context[namespaces[i]];
+        fields.each(function () {
+          const name = $(this).attr('name')
+          let id = $(this).attr('id')
+          const value = $(this).val()
+          if (jQuery.inArray(name, fieldsExcluded) === -1) {
+            if (id === undefined) {
+              id = name
             }
 
-            return context[func].apply(context, args);
-        }
+            fieldData[id] = value
+          }
+        })
 
-        return {
-            init: init,
-        };
+        button.html(labelWait)
+        button.attr('disabled', 'disabled')
 
-    })();
+        jQuery
+          .ajax({
+            type: 'POST',
+            url: ajaxurl,
+            dataType: 'json',
+            data: {
+              nonce: ajaxNonce,
+              action: 'rwps_ajax_button_' + button.attr('id'),
+              fields: fieldData
+            }
+          })
+          .fail(function (jqXHR, textStatus, errorThrown) {
+            if (labelError === '') {
+              if (
+                typeof jqXHR.responseJSON === 'object' &&
+                typeof jqXHR.responseJSON.data === 'string'
+              ) {
+                button.html(jqXHR.responseJSON.data)
+              } else {
+                button.html(buttonText)
+              }
+            } else {
+              button.html(labelError)
+            }
 
-    RWPSAjaxButtons.init();
-});
+            if (callbackError !== '' && callbackError !== undefined) {
+              executeCallback(
+                callbackError,
+                window,
+                jqXHR,
+                textStatus,
+                errorThrown
+              )
+            }
+          })
+          .done(function (response, textStatus, jqXHR) {
+            if (labelSuccess === '') {
+              if (typeof response.data === 'string') {
+                button.html(response.data)
+              } else {
+                button.html(buttonText)
+              }
+            } else {
+              button.html(labelSuccess)
+            }
+
+            if (callbackSuccess !== '' && callbackSuccess !== undefined) {
+              console.log('cb1', callbackSuccess)
+              executeCallback(
+                callbackSuccess,
+                window,
+                response,
+                textStatus,
+                jqXHR
+              )
+            }
+          })
+          .always(function (data, textStatus, errorThrown) {
+            setTimeout(function () {
+              button.removeAttr('disabled')
+              button.html(buttonText)
+            }, 3000)
+
+            if (callbackDone !== '' && callbackDone !== undefined) {
+              executeCallback(
+                callbackDone,
+                window,
+                data,
+                textStatus,
+                errorThrown
+              )
+            }
+          })
+      })
+    }
+
+    function executeCallback(callback, context) {
+      const args = Array.prototype.slice.call(arguments, 2)
+      const namespaces = callback.split('.')
+      const func = namespaces.pop()
+
+      for (let i = 0; i < namespaces.length; i++) {
+        context = context[namespaces[i]]
+      }
+
+      return context[func].apply(context, args)
+    }
+
+    return {
+      init
+    }
+  })()
+
+  RWPSAjaxButtons.init()
+})
